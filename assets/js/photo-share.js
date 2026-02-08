8236765e0536ec4f3d1b549c9f2dfec6b01280bd
federico.attygala <federico.attygala@consultant.rgigroup.com>
2026-02-08 14:36:02 +0100

    fixed instagram open and add new text for the reveal day

--- assets/js/photo-share.js
+++ assets/js/photo-share.js
@@ -2,11 +2,14 @@ function openInstagram() {
   const instagramApp = "instagram://camera";
   const instagramWeb = "https://www.instagram.com/";
 
-  // prova ad aprire l'app
-  window.location.href = instagramApp;
-
-  // fallback al web se non installata
-  setTimeout(() => {
-    window.open(instagramWeb, "_blank");
-  }, 800);
+  if (/iPhone|iPad|iPod|Android/.test(navigator.userAgent)) {
+    // prova ad aprire l'app
+    window.location.href = instagramApp;
+  } else {
+    // fallback al web se non installata
+      setTimeout(() => {
+        window.open(instagramWeb, "_blank");
+      }, 800);
+  }
+  
 }
